98fa4cd2bb188dcba2e4ae9015e7d070e4de5849
Mohamad Shiralizadeh <mohamad.shir@gmail.com>
2023-03-27 16:58:56 +0200

    Multi-line paste feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,53 @@
+import { type } from "os";
 import * as vscode from "vscode";
 import { TextEditorEdit } from "vscode";
 
 const KEY = "cycle-clipboard-ring";
+const MAX_HISTORY_SIZE = 10;
+
+type ClipboardItem = {
+  text: string;
+  isSingleLine: boolean;
+  length: number;
+  lines: number;
+};
 
 export function activate(context: vscode.ExtensionContext) {
-  const clipboardRing: string[] = [];
+  const clipboardRingItems: ClipboardItem[] = [];
   let clipboardRingIndex: number = -1;
 
-  const addToClipboardRing = (text: string) => {
-    if (clipboardRing[clipboardRing.length - 1] !== text) {
-      clipboardRing.push(text);
-      clipboardRingIndex = clipboardRing.length;
+  const addToClipboardRing = (text: string, selection: vscode.Selection) => {
+    if (clipboardRingItems[clipboardRingItems.length - 1]?.text !== text) {
+      if (clipboardRingItems.length === MAX_HISTORY_SIZE) {
+        clipboardRingItems.shift(); // remove first element
+      }
+
+      clipboardRingItems.push({
+        text,
+        isSingleLine: selection.isSingleLine,
+        length: selection.isSingleLine
+          ? Math.abs(selection.end.character - selection.start.character)
+          : selection.end.character,
+        lines: selection.isSingleLine
+          ? 0
+          : Math.abs(selection.end.line - selection.start.line),
+      });
+      clipboardRingIndex = clipboardRingItems.length;
     }
   };
 
-  const nextClipboardRing = async () => {
-    if (!clipboardRing.length) {
-      return await vscode.env.clipboard.readText();
+  const nextClipboardRing = async (): Promise<ClipboardItem | undefined> => {
+    if (!clipboardRingItems.length) {
+      return;
     }
 
     clipboardRingIndex--;
 
     if (clipboardRingIndex === -1) {
-      clipboardRingIndex = clipboardRing.length - 1;
+      clipboardRingIndex = clipboardRingItems.length - 1;
     }
 
-    return clipboardRing[clipboardRingIndex];
+    return clipboardRingItems[clipboardRingIndex];
   };
 
   const copyDisposable = vscode.commands.registerCommand(`${KEY}.copy`, (e) => {
@@ -35,7 +57,7 @@ export function activate(context: vscode.ExtensionContext) {
       const text = activeEditor.document.getText(activeEditor.selection);
 
       vscode.env.clipboard.writeText(text);
-      addToClipboardRing(text);
+      addToClipboardRing(text, activeEditor.selection);
     }
   });
 
@@ -64,11 +86,38 @@ export function activate(context: vscode.ExtensionContext) {
       const activeEditor = vscode.window.activeTextEditor;
 
       if (activeEditor) {
-        const text = await nextClipboardRing();
+        const clipboardItem = await nextClipboardRing();
+        let selection: vscode.Selection | undefined = undefined;
 
-        activeEditor.edit(function (editBuilder: TextEditorEdit) {
-          editBuilder.replace(activeEditor.selection, text); // Replace currently selected
-        });
+        if (!clipboardItem) {
+          return;
+        }
+
+        activeEditor
+          .edit(function (editBuilder: TextEditorEdit) {
+            selection = new vscode.Selection(
+              new vscode.Position(
+                activeEditor.selection.start.line,
+                activeEditor.selection.start.character
+              ),
+              new vscode.Position(
+                activeEditor.selection.start.line + clipboardItem.lines,
+                clipboardItem.isSingleLine
+                  ? activeEditor.selection.start.character +
+                    clipboardItem.length
+                  : clipboardItem.length
+              )
+            );
+
+            editBuilder.replace(activeEditor.selection, clipboardItem.text); // Replace currently selected
+          })
+          .then(() => {
+            if (selection) {
+              console.log(selection);
+
+              activeEditor.selection = selection;
+            }
+          });
       }
     }
   );
